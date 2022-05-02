d6471f7f3434ced62ae03d539bdafbcb8710ab05
Fred Sonnenrein <fred.sonnenrein@gmx.de>
2022-05-02 18:56:13 +0000

    Fastboot updated for mega1284p
    git-svn-id: svn://svn.code.sf.net/p/itelex/code-0/trunk@945 eda8b1fe-747e-4470-9eba-51160ea1b311

--- FastBoot/atmel_def.h
+++ FastBoot/atmel_def.h
@@ -1,18 +1,18 @@
 #define  SIGNATURE_000 0x1e
-#define  SIGNATURE_001 0x98
-#define  SIGNATURE_002 0x02
+#define  SIGNATURE_001 0x97
+#define  SIGNATURE_002 0x05
 ; ***** BOOT_LOAD ********************
 #define  BOOTRST 0	// Select Reset Vector
 #define  BOOTSZ0 1	// Select Boot Size
 #define  BOOTSZ1 2	// Select Boot Size
-#define  FLASHEND 0x1ffff	// Note: Word address
-#define  SRAM_START 0x0200
-#define  SRAM_SIZE 8192
+#define  FLASHEND 0xffff	// Note: Word address
+#define  SRAM_START 0x0100
+#define  SRAM_SIZE 16384
 ; ***** BOOTLOADER DECLARATIONS ******************************************
 #define  PAGESIZE 128
-#define  FIRSTBOOTSTART 0x1fe00
-#define  SECONDBOOTSTART 0x1fc00
-#define  THIRDBOOTSTART 0x1f800
-#define  FOURTHBOOTSTART 0x1f000
+#define  FIRSTBOOTSTART 0xfe00
+#define  SECONDBOOTSTART 0xfc00
+#define  THIRDBOOTSTART 0xf800
+#define  FOURTHBOOTSTART 0xf000
 #define  SMALLBOOTSTART FIRSTBOOTSTART
 #define  LARGEBOOTSTART FOURTHBOOTSTART
