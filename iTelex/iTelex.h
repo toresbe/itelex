1b8fa8165724761952a86b41ae79acf4dd180436
Fred Sonnenrein <fred.sonnenrein@gmx.de>
2022-07-20 20:02:10 +0000

    Korrektur RemoteServerPprtDefault
    git-svn-id: svn://svn.code.sf.net/p/itelex/code-0/branch/22-04_Light@960 eda8b1fe-747e-4470-9eba-51160ea1b311

--- iTelex/iTelex.h
+++ iTelex/iTelex.h
@@ -101,7 +101,7 @@ enum {
 	ITELEX_TLNSERV_PORT = 11811,
 	//!< Der TCP-Port für die Kommunikation mit den Teilnehmer-Servern.
 
-	RemoteServerPort = 49491,
+	RemoteServerPortDefault = 49491,
 	//!< Der TCP-Port für die Kommunikation mit dem "RemoteServer" (= "Centralex" = "
 	
 	TlnAdresseMax = 40,
